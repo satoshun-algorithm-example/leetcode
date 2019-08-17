38585ec4dec5f52a4affeab07897e6f66b05445f
SatoShun <shun.sato1@gmail.com>
2019-08-17 13:51:24 +0000

    diameter

--- 543.diameter-of-binary-tree.py
+++ 543.diameter-of-binary-tree.py
@@ -12,21 +12,14 @@
 
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        if root is None:
-            return 0
+        self.ans = 0
 
-        a = []
-        if root.left is not None:
-            a.append(self.max_depth(root.left, 1))
-        if root.right is not None:
-            a.append(self.max_depth(root.right, 1))
+        def depth(p):
+            if not p:
+                return 0
+            left, right = depth(p.left), depth(p.right)
+            self.ans = max(self.ans, left + right)
+            return 1 + max(left, right)
 
-        return sum(a)
-
-    def max_depth(self, node, depth):
-        a = [depth]
-        if node.left is not None:
-            a.append(self.max_depth(node.left, depth + 1))
-        if node.right is not None:
-            a.append(self.max_depth(node.right, depth + 1))
-        return max(a)
+        depth(root)
+        return self.ans
