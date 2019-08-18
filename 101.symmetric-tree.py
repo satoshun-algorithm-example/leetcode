60425c4b137b02498647bc2146a92d79969a7ed6
SatoShun <shun.sato1@gmail.com>
2019-08-18 08:44:15 +0000

    symmetric tree

---
+++ 101.symmetric-tree.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=101 lang=python3
+#
+# [101] Symmetric Tree
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        return self.symmentric(root.left, root.right)
+
+    def symmentric(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        if left.val != right.val:
+            return False
+
+        return self.symmentric(left.left, right.right) and self.symmentric(left.right, right.left)
