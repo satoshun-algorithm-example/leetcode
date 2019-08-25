e217f8565ef7350dfd6b759d0c167fc641846af1
SatoShun <shun.sato1@gmail.com>
2019-08-25 02:19:59 +0000

    more simple

--- 409.longest-palindrome.py
+++ 409.longest-palindrome.py
@@ -5,12 +5,10 @@
 #
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        is_odd = False
-        total = 0
+        odd = 0
         for c in set(s):
             count = s.count(c)
             rest = count % 2
-            total += count - rest
             if rest != 0:
-                is_odd = True
-        return total if not is_odd else total + 1
+                odd += 1
+        return len(s) - odd + 1 if odd else len(s)
