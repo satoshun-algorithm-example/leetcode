74574763d790f4aa127382fe68772b022473a691
SatoShun <shun.sato1@gmail.com>
2019-08-28 00:08:13 +0000

    factorial 5

--- 172.factorial-trailing-zeroes.py
+++ 172.factorial-trailing-zeroes.py
@@ -5,20 +5,7 @@
 #
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        def fuctorial(v):
-            i = 1
-            for b in range(2, v + 1):
-                i *= b
-            return i
-
-        f = fuctorial(n)
-
-        def countup(s):
-            i = 0
-            for c in s[::-1]:
-                if c != '0':
-                    break
-                i += 1
-            return i
-
-        return countup(str(f))
+        i = n // 5
+        if i == 0:
+            return 0
+        return i + self.trailingZeroes(i)
