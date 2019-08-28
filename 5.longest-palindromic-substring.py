a4ba4d3ea06d6306e5dcb62c07cbe01a254ff8cf
SatoShun <shun.sato1@gmail.com>
2019-08-28 23:01:49 +0000

    only odd case

--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -7,8 +7,28 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if not s:
             return ""
+
         r = s[0]
-        for i in range(len(s)):
-            pass
+        ml = 1
+        for i in range(1, len(s) - 1):
+            cl = 0
+
+            while True:
+                left = i - cl
+                right = i + cl
+                if left < 0:
+                    break
+                if right >= len(s):
+                    break
+                if s[left] != s[right]:
+                    break
+                cl += 1
+
+            cl -= 1
+            if ml < (cl + 1) * 2 - 1:
+                left = i - cl
+                right = i + cl
+                r = s[left:right + 1]
+                ml = len(r)
 
         return r
