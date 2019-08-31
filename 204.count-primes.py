580f5b51bc04eb1030fec9ceb5470aea5f4f515d
SatoShun <shun.sato1@gmail.com>
2019-08-31 03:06:56 +0000

    count primes

--- 204.count-primes.py
+++ 204.count-primes.py
@@ -5,11 +5,37 @@
 #
 class Solution:
     def countPrimes(self, n: int) -> int:
+        if n < 3:
+            return 0
         primes = range(2, n)
+        u = n ** 0.5
         i = 0
-        while primes:
+        while primes[0] < u:
             prime = primes[0]
             primes = [p for p in primes if p % prime != 0]
             i += 1
+        return i + len(primes)
 
-        return i
+
+def countPrimes2(n: int) -> int:
+    if n < 3:
+        return 0
+    primes = range(2, n)
+    u = n ** 0.5
+    i = 0
+    while primes[0] < u:
+        prime = primes[0]
+        primes = [p for p in primes if p % prime != 0]
+        i += 1
+    return i + len(primes)
+
+
+def countPrimes1(n: int) -> int:
+    primes = range(2, n)
+    i = 0
+    while primes:
+        prime = primes[0]
+        primes = [p for p in primes if p % prime != 0]
+        i += 1
+
+    return i
