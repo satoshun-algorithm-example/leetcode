d94582530a034458621cb1d2dc44593b2412146d
SatoShun <shun.sato1@gmail.com>
2019-09-01 23:03:09 +0000

    reverse string

---
+++ 344.reverse-string.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=344 lang=python3
+#
+# [344] Reverse String
+#
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start = 0
+        end = len(s) - 1
+        while start <= end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
