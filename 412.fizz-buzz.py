51405fc3a8a82fbca65bd0d704f19975ed38cc4f
SatoShun <shun.sato1@gmail.com>
2019-09-02 00:07:24 +0000

    short

--- 412.fizz-buzz.py
+++ 412.fizz-buzz.py
@@ -8,14 +8,5 @@ from typing import List
 
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        r = []
-        for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
-                r.append('FizzBuzz')
-            elif i % 3 == 0:
-                r.append('Fizz')
-            elif i % 5 == 0:
-                r.append('Buzz')
-            else:
-                r.append(str(i))
-        return r
+        return [('Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0)) or str(i)
+                for i in range(1, n + 1)]
