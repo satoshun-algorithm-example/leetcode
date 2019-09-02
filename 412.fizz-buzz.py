7e1b565138c420f86966f7773274d4fe55214044
SatoShun <shun.sato1@gmail.com>
2019-09-02 00:05:04 +0000

    fizz buzz

---
+++ 412.fizz-buzz.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=412 lang=python3
+#
+# [412] Fizz Buzz
+#
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        r = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                r.append('FizzBuzz')
+            elif i % 3 == 0:
+                r.append('Fizz')
+            elif i % 5 == 0:
+                r.append('Buzz')
+            else:
+                r.append(str(i))
+        return r
