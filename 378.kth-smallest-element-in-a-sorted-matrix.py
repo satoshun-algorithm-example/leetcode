ba38e4c86eae3b2c78aa0359287cbb7f95c65680
SatoShun <shun.sato1@gmail.com>
2019-09-12 02:05:27 +0000

    TLE

---
+++ 378.kth-smallest-element-in-a-sorted-matrix.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=378 lang=python3
+#
+# [378] Kth Smallest Element in a Sorted Matrix
+#
+from typing import List
+
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        indices = [0 for _ in range(len(matrix))]
+
+        while True:
+            next_value = -1
+            next_i = -1
+
+            for i, index in enumerate(indices):
+                m = matrix[i]
+                if len(m) <= index:
+                    continue
+                value = m[index]
+                if next_value == -1 or value < next_value:
+                    next_value = value
+                    next_i = i
+
+            indices[next_i] += 1
+            if sum(indices) == k:
+                return next_value
