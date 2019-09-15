50cb68f5b4bf244fd71cea2bf149ac77d1bdc783
SatoShun <shun.sato1@gmail.com>
2019-09-15 12:11:42 +0000

    more efficiennt

--- 64.minimum-path-sum.py
+++ 64.minimum-path-sum.py
@@ -15,21 +15,19 @@ class Solution:
         board = [[sys.maxsize] * xm for _ in range(ym)]
 
         board[0][0] = grid[0][0]
+        queue = [(grid[0][0], 0, 0)]
         while True:
-            i, j = 0, 0
-            m = sys.maxsize
-            for y in range(ym):
-                for x in range(xm):
-                    if board[y][x] != -1 and board[y][x] < m:
-                        i, j = y, x
-                        m = board[y][x]
+            q = queue.pop(0)
 
-            if i == ym - 1 and j == xm - 1:
-                return m
-
-            board[i][j] = -1
+            m, i, j = q[0], q[1], q[2]
+            if q[1] == ym - 1 and q[2] == xm - 1:
+                return q[0]
 
             if j + 1 < xm and m + grid[i][j + 1] < board[i][j + 1]:
                 board[i][j + 1] = m + grid[i][j + 1]
+                queue.append((board[i][j + 1], i, j + 1))
             if i + 1 < ym and m + grid[i + 1][j] < board[i + 1][j]:
                 board[i + 1][j] = m + grid[i + 1][j]
+                queue.append((board[i + 1][j], i + 1, j))
+
+            queue.sort()
