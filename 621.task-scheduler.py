273bf42aae4a758d3c71fcfec99202204a4d9936
SatoShun <shun.sato1@gmail.com>
2019-09-16 04:01:29 +0000

    task scheduler

---
+++ 621.task-scheduler.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=621 lang=python3
+#
+# [621] Task Scheduler
+#
+from typing import List
+
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        pass
