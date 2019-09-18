f7619a68cc763c4f38e82791f9d53f21961312af
SatoShun <shun.sato1@gmail.com>
2019-09-18 06:10:50 +0000

    manager 2

--- 181.employees-earning-more-than-their-managers.sql
+++ 181.employees-earning-more-than-their-managers.sql
@@ -1,7 +1,3 @@
-SELECT Name as Employee FROM Employee as e1
- WHERE EXISTS(
-    SELECT * FROM Employee as e2
-        WHERE
-            e2.Id = e1.ManagerId AND
-            e2.Salary < e1.Salary
-    );
+SELECT e1.Name as Employee FROM Employee as e1
+ JOIN Employee as e2 ON e1.ManagerId = e2.Id
+ WHERE e2.Salary < e1.Salary;
