3aeba64e672b6dc0a709c012acb8692acf3028f5
SatoShun <shun.sato1@gmail.com>
2019-09-18 10:19:14 +0000

    rank scores

--- 178.rank-scores.sql
+++ 178.rank-scores.sql
@@ -1,5 +1,5 @@
 SELECT
     s.Score,
-    (SELECT COUNT(distinct Score) FROM Scores WHERE s.Score <= Score) as Rank
+    (SELECT COUNT(*) FROM (SELECT distinct Score FROM Scores) tmp WHERE s.Score <= Score) as Rank
     FROM Scores as s
     ORDER BY s.Score DESC;
