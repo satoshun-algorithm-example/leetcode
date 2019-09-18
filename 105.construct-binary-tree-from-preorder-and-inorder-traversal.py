c045c6ce83d138d8213abf39e47778ab0b9c2793
SatoShun <shun.sato1@gmail.com>
2019-09-18 04:11:20 +0000

    105

---
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=105 lang=python3
+#
+# [105] Construct Binary Tree from Preorder and Inorder Traversal
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from typing import List
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        pass
