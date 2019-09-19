1bd50089ac833a1b180f83359e34ab896ab8f670
SatoShun <shun.sato1@gmail.com>
2019-09-19 00:32:30 +0000

    deparment

--- 185.department-top-three-salaries.sql
+++ 185.department-top-three-salaries.sql
@@ -1,9 +1,10 @@
 SELECT d.Name as Department, e.Name as Employee, e.Salary as Salary
  FROM Employee as e
  INNER JOIN Department as d ON d.Id = e.DepartmentId
- WHERE e.Salary IN (
-    SELECT Salary FROM Employee
-    WHERE DepartmentId = e.DepartmentId
-    ORDER BY Salary DESC
-    LIMIT 3
-);
+ WHERE 3 > (
+    SELECT COUNT(DISTINCT Salary) FROM Employee
+    WHERE
+        DepartmentId = e.DepartmentId AND
+        Salary > e.Salary
+ )
+ ORDER BY d.Name, e.Salary DESC;
