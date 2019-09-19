0479a66b8f9d02b307b033b1ebdd2d4a38eddffa
SatoShun <shun.sato1@gmail.com>
2019-09-19 06:57:24 +0000

    dynamic + comprehension list

--- 279.perfect-squares.py
+++ 279.perfect-squares.py
@@ -15,7 +15,7 @@ class Solution:
         dp = [sys.maxsize for _ in range(n + 1)]
         dp[0] = 0
         for i in range(1, n + 1):
-            for j in range(1, int(math.sqrt(i)) + 1):
-                dp[i] = min(dp[i], dp[i - j * j] + 1)
+            dp[i] = min(dp[i - j * j] + 1
+                        for j in range(1, int(math.sqrt(i)) + 1))
 
         return dp[n]
