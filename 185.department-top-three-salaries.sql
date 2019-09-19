b5510a37d205522f87a71c23e7422f817fc28c15
SatoShun <shun.sato1@gmail.com>
2019-09-19 00:19:24 +0000

    subquery

--- 185.department-top-three-salaries.sql
+++ 185.department-top-three-salaries.sql
@@ -1,8 +1,9 @@
-SELECT d.Name as Salary, e.Name as Employee, e.Salary as Salary
+SELECT d.Name as Department, e.Name as Employee, e.Salary as Salary
  FROM Employee as e
  INNER JOIN Department as d ON d.Id = e.DepartmentId
  WHERE e.Salary IN (
     SELECT Salary FROM Employee
+    WHERE DepartmentId = e.DepartmentId
     ORDER BY Salary DESC
     LIMIT 3
 );
