ebfefab510d6b360535acffd44656033333e190e
SatoShun <shun.sato1@gmail.com>
2019-09-21 08:21:55 +0000

    dp

--- 416.partition-equal-subset-sum.py
+++ 416.partition-equal-subset-sum.py
@@ -4,18 +4,19 @@
 # [416] Partition Equal Subset Sum
 #
 from typing import List
-from itertools import combinations
 
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        d = {i: num for i, num in enumerate(nums)}
-        numbers = [i for i in range(len(nums))]
-        for i in range(1, len(nums)):
-            combs = combinations(numbers, i)
-            for comb1 in combs:
-                comb2 = [num for i, num in enumerate(nums) if i not in comb1]
-                comb1 = [d.get(c) for c in comb1]
-                if sum(comb1) == sum(comb2):
-                    return True
-        return False
+        if not nums:
+            return True
+
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+
+        total = total // 2
+        dp = [False for _ in range(total + 1)]
+        dp[0] = True
+
+        return dp[total]
