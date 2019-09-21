1c9c5f61053e6f1c988b48687ecff8ce889c0a4d
SatoShun <shun.sato1@gmail.com>
2019-09-21 15:02:29 +0000

    remove adjacent

--- 1047.remove-all-adjacent-duplicates-in-string.py
+++ 1047.remove-all-adjacent-duplicates-in-string.py
@@ -5,15 +5,11 @@
 #
 class Solution:
     def removeDuplicates(self, S: str) -> str:
-        while True:
-            find = False
-            for (c1, c2) in zip(S[:len(S)], S[1:]):
-                if c1 == c2:
-                    find = True
-                    S = S.replace(c1 + c2, "", 1)
-                    break
+        stack = []
+        for c in S:
+            if stack and stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
 
-            if not find:
-                break
-
-        return S
+        return "".join(stack)
