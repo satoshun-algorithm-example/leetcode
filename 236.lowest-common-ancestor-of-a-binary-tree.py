f024310192e6688bb57510ef4d7befa282abf3a2
SatoShun <shun.sato1@gmail.com>
2019-09-24 10:16:28 +0000

    common ancestor

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=236 lang=python3
+#
+# [236] Lowest Common Ancestor of a Binary Tree
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        # fast path
+        is_q = self.same_path(q, p)
+        if is_q:
+            return is_q
+
+        while True:
+            is_p = self.same_path(p, q)
+            if is_p:
+                return is_p
+
+            p = self.get_parent(root, p)
+
+    def get_parent(self, node, p):
+        if not node:
+            return None
+
+        if node.left:
+            if node.left.val == p.val:
+                return node
+
+        if node.right:
+            if node.right.val == p.val:
+                return node
+
+        return self.get_parent(node.left, p) or self.get_parent(node.right, p)
+
+    def same_path(self, node, target):
+        if not node:
+            return None
+
+        if node.val == target.val:
+            return node
+
+        return self.same_path(node.left, target) or self.same_path(node.right, target)
