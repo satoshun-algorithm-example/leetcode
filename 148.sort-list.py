344a203ac3c41fa0e15d2a4bcf7a884155467dcf
SatoShun <shun.sato1@gmail.com>
2019-09-25 09:57:40 +0000

    sort

---
+++ 148.sort-list.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode id=148 lang=python3
+#
+# [148] Sort List
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+
+        last = head
+        count = 1
+        while last.next:
+            last = last.next
+            count += 1
+
+        head = self.sort(head, last)
+        return head
+
+    def sort(self, head, last):
+        if not head or not last:
+            return head
+        if head.val == last.val:
+            return head
+        before = None
+        current = head
+        new_last = last
+        while current and current.val != last.val:
+            if last.val < current.val:
+                if not before:
+                    head = current.next
+                    current.next = None
+                else:
+                    before.next = current.next
+                new_last.next = current
+                new_last = current
+            else:
+                before = current
+            current = current.next
+
+        self.sort(head, before)
+        self.sort(last, new_last)
+
+        return head
