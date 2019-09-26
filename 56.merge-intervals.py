54ce4c51af70d229fc1bc87e7689aa4c7648c3f1
SatoShun <shun.sato1@gmail.com>
2019-09-26 02:48:27 +0000

    merge intervals

---
+++ 56.merge-intervals.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=56 lang=python3
+#
+# [56] Merge Intervals
+#
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        i = 0
+        while len(intervals) > i:
+            target = intervals[i]
+            i += 1
+            while len(intervals) > i and target[1] > intervals[i][0]:
+                target[1] = max(target[1], intervals[i][1])
+                i += 1
+
+            res.append(target)
+        return res
