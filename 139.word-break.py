93f4069142e6065196b79fb07e199672ccfb712e
SatoShun <shun.sato1@gmail.com>
2019-09-26 03:35:33 +0000

    add cache

--- 139.word-break.py
+++ 139.word-break.py
@@ -12,15 +12,20 @@ class Solution:
         for word in wordDict:
             d[word] = d.get(word, 0) + 1
 
-        return self.find_word(s, 0, d)
+        cache = []
+        return self.find_word(s, 0, d, cache)
 
-    def find_word(self, s, start, rest_words):
+    def find_word(self, s, start, rest_words, cache):
         if len(s) == start:
             return True
 
+        if start in cache:
+            return False
+        cache.append(start)
+
         for word in rest_words.keys():
             if s[start:start + len(word)] == word:
-                if self.find_word(s, start + len(word), rest_words):
+                if self.find_word(s, start + len(word), rest_words, cache):
                     return True
 
         return False
