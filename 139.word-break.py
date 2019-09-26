6f04c23de734fdc246b45bb5259fefd4a068b20a
SatoShun <shun.sato1@gmail.com>
2019-09-26 03:33:56 +0000

    word break TLE

--- 139.word-break.py
+++ 139.word-break.py
@@ -8,28 +8,19 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        words_len = sum(len(word) for word in wordDict)
         d = {}
         for word in wordDict:
             d[word] = d.get(word, 0) + 1
-        for i in range(len(s) - words_len + 1):
-            dd = d.copy()
-            if self.find_word(s, i, i + words_len, dd):
-                return True
 
-        return False
+        return self.find_word(s, 0, d)
 
-    def find_word(self, s, start, end, rest_words):
-        if not rest_words:
+    def find_word(self, s, start, rest_words):
+        if len(s) == start:
             return True
 
         for word in rest_words.keys():
             if s[start:start + len(word)] == word:
-                words = rest_words.copy()
-                words[word] -= 1
-                if words[word] == 0:
-                    del words[word]
-                if self.find_word(s, start + len(word), end, words):
+                if self.find_word(s, start + len(word), rest_words):
                     return True
 
         return False
