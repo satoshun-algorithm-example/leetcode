7f5b160df452280976035d215ed0a8ed65fee229
SatoShun <shun.sato1@gmail.com>
2019-09-29 07:01:02 +0000

    search rotated

--- 33.search-in-rotated-sorted-array.py
+++ 33.search-in-rotated-sorted-array.py
@@ -8,8 +8,22 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i, n in enumerate(nums):
-            if n == target:
-                return i
+        if not nums:
+            return -1
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
 
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
         return -1
