04f7089f6c777db6ebfd821590367d630d8aed26
SatoShun <shun.sato1@gmail.com>
2019-10-08 00:57:03 +0000

    house robber

--- 337.house-robber-iii.py
+++ 337.house-robber-iii.py
@@ -12,4 +12,21 @@
 
 class Solution:
     def rob(self, root: TreeNode) -> int:
-        pass
+        dfs = [0, 0]
+        stack = [root]
+        while stack:
+            new_stack = []
+            value = 0
+            for node in stack:
+                value += node.val
+                if node.left:
+                    new_stack.append(node.left)
+                if node.right:
+                    new_stack.append(node.right)
+
+            l = len(dfs)
+            dfs.append(max(dfs[l - 1], dfs[l - 2] + value))
+
+            stack = new_stack
+
+        return dfs[-1]
