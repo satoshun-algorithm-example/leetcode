96f620de2a8c49660aae6943b1142dd7dc1ea7bc
SatoShun <shun.sato1@gmail.com>
2019-10-08 23:46:07 +0000

    sum of two

--- 371.sum-of-two-integers.py
+++ 371.sum-of-two-integers.py
@@ -5,4 +5,21 @@
 #
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        pass
+        sa = str(a)
+        sb = str(b)
+        if len(sa) > len(sb):
+            sb = '0' * (len(sa) - len(sb)) + sb
+        else:
+            sa = '0' * (len(sb) - len(sa)) + sa
+
+        sa, sb = list(reversed(sa)), list(reversed(sb))
+        s = 0
+        carry = 0
+        for i in range(len(sa)):
+            d = int(sa[i]) ^ int(sb[i])
+            d = d ^ carry
+
+            carry = (sa[i] and sb[i]) or (sa[i] and carry) or (sb[i] and carry)
+            s = s or (d << i)
+
+        return s
