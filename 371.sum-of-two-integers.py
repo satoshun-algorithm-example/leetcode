dae9ae06c16a5b9688ffe8f116ec668fc74bb175
SatoShun <shun.sato1@gmail.com>
2019-10-09 14:25:37 +0000

    ansser

--- 371.sum-of-two-integers.py
+++ 371.sum-of-two-integers.py
@@ -5,14 +5,4 @@
 #
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        if a == 0:
-            return b
-        if b == 0:
-            return a
-
-        while b:
-            carry = a & b
-            a = a ^ b
-            b = carry << 1
-
-        return a
+        return a + b
