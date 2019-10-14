e7b1b2f637b0bc20f1f12f846dbd43ffe62d83e6
SatoShun <shun.sato1@gmail.com>
2019-10-14 06:27:04 +0000

    partition

---
+++ 1043.partition-array-for-maximum-sum.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=1043 lang=python3
+#
+# [1043] Partition Array for Maximum Sum
+#
+from typing import List
+
+
+# @lc code=start
+class Solution:
+    def maxSumAfterPartitioning(self, A: List[int], K: int) -> int:
+        pass
+# @lc code=end
