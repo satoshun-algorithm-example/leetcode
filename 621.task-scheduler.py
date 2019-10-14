2392f0c6f9bcf6c401bab2886dcff20600c54bce
SatoShun <shun.sato1@gmail.com>
2019-10-14 23:55:31 +0000

    task scheduler

--- 621.task-scheduler.py
+++ 621.task-scheduler.py
@@ -8,4 +8,17 @@ from typing import List
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        pass
+        count = [0] * 26
+        for c in tasks:
+            count[ord(c) - ord('A')] += 1
+
+        count.sort(reverse=True)
+
+        i = 0
+        while i < 26 and count[i] == count[0]:
+            i += 1
+
+        return max(
+            len(tasks),
+            (count[0] - 1) * (n + 1) + i
+        )
