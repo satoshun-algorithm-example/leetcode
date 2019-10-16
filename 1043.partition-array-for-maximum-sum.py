7a7b75da54b2c2c52bc7a9512df92ef53a40de4e
SatoShun <shun.sato1@gmail.com>
2019-10-16 00:40:04 +0000

    maximum

--- 1043.partition-array-for-maximum-sum.py
+++ 1043.partition-array-for-maximum-sum.py
@@ -17,13 +17,13 @@ class Solution:
         dp = [0] * len(A)
         dp[0] = A[0]
         for i in range(1, len(A)):
-            m = dp[i - 1] + A[i]
+            m = 0
             for j in range(K):
                 if i - j - 1 < 0:
+                    dp[i] = max(dp[i], (j + 1) * m)
                     break
-
-                m = max(m, dp[i - j - 1] + (j + 1) * A[i])
-            dp[i] = m
+                m = max(m, A[i - j])
+                dp[i] = max(dp[i], dp[i - j - 1] + (j + 1) * m)
 
         return dp[-1]
 # @lc code=end
