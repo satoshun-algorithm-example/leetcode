f8fbd1ee5b0a7fdea4faf35fbe62fb14da1f1831
SatoShun <shun.sato1@gmail.com>
2019-10-17 00:38:03 +0000

    890 find and replace

--- 890.find-and-replace-pattern.py
+++ 890.find-and-replace-pattern.py
@@ -17,26 +17,27 @@ class Solution:
 
         res = []
         for word in words:
+            is_match = True
+            checked_char = set()
+
             for group in groups:
                 before = None
-                is_not_match = False
-                checked_char = set()
 
                 for index in group:
-                    if before is not None or word[index] != before:
-                        is_not_match = True
+                    if before is not None and word[index] != before:
+                        is_match = False
                         break
                     if before is None:
                         before = word[index]
                         if before in checked_char:
-                            is_not_match = True
+                            is_match = False
                             break
                         checked_char.add(before)
                     else:
                         before = word[index]
 
-                if not is_not_match:
-                    res.append(word)
+            if is_match:
+                res.append(word)
 
         return res
 # @lc code=end
