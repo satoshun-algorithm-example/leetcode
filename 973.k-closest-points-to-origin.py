47087da0e5d10952650a0a02ae49e7eeb1ada71c
SatoShun <shun.sato1@gmail.com>
2019-10-18 00:28:34 +0000

    more simplify

--- 973.k-closest-points-to-origin.py
+++ 973.k-closest-points-to-origin.py
@@ -9,10 +9,6 @@ from typing import List
 # @lc code=start
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        distances = [
-            (point[0] ** 2 + point[1] ** 2, point) for point in points
-        ]
-        distances.sort()
-        return [distance[1] for distance in distances][:K]
+        return sorted(points, key=lambda point: point[0] ** 2 + point[1] ** 2)[:K]
 
 # @lc code=end
