de5aa577aa81486fdacb76d23ddfd0690052f934
SatoShun <shun.sato1@gmail.com>
2019-10-25 00:53:44 +0000

    partition

--- 416.partition-equal-subset-sum.py
+++ 416.partition-equal-subset-sum.py
@@ -8,18 +8,21 @@ from typing import List
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if not nums:
-            return True
-
-        total = sum(nums)
-        if total % 2 == 1:
+        value = sum(nums) // 2
+        if value % 2 == 1:
             return False
 
-        total = total // 2
-        dp = [False for _ in range(total + 1)]
-        dp[0] = True
-        for i in range(1, len(nums)):
-            for j in range(total, nums[i] - 1, -1):
-                dp[j] = dp[j] or dp[j - nums[i]]
+        dp = []
+        for i in range(len(nums) + 1):
+            dp.append([False for _ in range(value + 1)])
 
-        return dp[total]
+        for i in range(len(nums) + 1):
+            dp[i][0] = True
+
+        for i in range(1, len(nums) + 1):
+            for j in range(1, value + 1):
+                dp[i][j] = dp[i - 1][j]
+                if j >= nums[i - 1]:
+                    dp[i][j] = dp[i][j] or dp[i - 1][j - nums[i - 1]]
+
+        return dp[len(nums)][value]
