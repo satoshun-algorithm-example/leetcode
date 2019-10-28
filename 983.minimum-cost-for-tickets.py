63ea50cad9643c4988719f983ee3349524d044bc
SatoShun <shun.sato1@gmail.com>
2019-10-28 00:51:38 +0000

    minimum cost

--- 983.minimum-cost-for-tickets.py
+++ 983.minimum-cost-for-tickets.py
@@ -19,19 +19,28 @@ class Solution:
                 c2 = costs[1]
                 c3 = costs[2]
             else:
-                c1 = dp[i - 1] + costs[0]
+                ci1 = day - 1
+                while dp.get(ci1) is None:
+                    ci1 -= 1
+                c1 = dp[ci1] + costs[0]
 
                 ci2 = min(i - 6, 0)
                 while dp.get(ci2) is None:
                     ci2 += 1
-                c2 = dp[ci2] + costs[1]
+                if ci2 == day:
+                    c2 = dp[ci1] + costs[1]
+                else:
+                    c2 = dp[ci2] + costs[1]
 
                 ci3 = min(i - 29, 0)
                 while dp.get(ci3) is None:
                     ci3 += 1
-                c3 = dp[ci3] + costs[2]
+                if ci3 == day:
+                    c3 = dp[ci1] + costs[2]
+                else:
+                    c3 = dp[ci3] + costs[2]
 
-            dp[i] = min(c1, c2, c3)
+            dp[day] = min(c1, c2, c3)
 
         return dp[days[-1]]
 
