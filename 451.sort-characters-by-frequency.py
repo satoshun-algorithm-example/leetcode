65b9ed2b0faefe369d3777ae63ff4a5df5d67644
SatoShun <shun.sato1@gmail.com>
2019-10-29 00:18:25 +0000

    sort characters

--- 451.sort-characters-by-frequency.py
+++ 451.sort-characters-by-frequency.py
@@ -7,5 +7,12 @@
 # @lc code=start
 class Solution:
     def frequencySort(self, s: str) -> str:
-        return "".join(sorted(s, key=lambda x: (s.count(x), x), reverse=True))
+        cache = {}
+
+        def key(x):
+            if x in cache:
+                return cache[x], x
+            return cache.setdefault(x, s.count(x)), x
+
+        return "".join(sorted(s, key=key, reverse=True))
 # @lc code=end
