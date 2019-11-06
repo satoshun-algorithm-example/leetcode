59eff3006dac251170c5a6d0bd28b39cdfc625b8
SatoShun <shun.sato1@gmail.com>
2019-11-06 23:46:25 +0000

    minimum ascii

--- 712.minimum-ascii-delete-sum-for-two-strings.py
+++ 712.minimum-ascii-delete-sum-for-two-strings.py
@@ -7,18 +7,18 @@
 # @lc code=start
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        dp = [[0] * len(s1) for _ in s2]
-        for i in range(1, len(s1)):
-            dp[0][i] = dp[0][i - 1] + ord(s1[i])
-        for i in range(1, len(s2)):
-            dp[i][0] = dp[i - 1][0] + ord(s2[i])
+        dp = [[0] * (len(s1) + 1) for _ in range(len(s2) + 1)]
+        for i in range(1, len(s1) + 1):
+            dp[0][i] = dp[0][i - 1] + ord(s1[i - 1])
+        for j in range(1, len(s2) + 1):
+            dp[j][0] = dp[j - 1][0] + ord(s2[j - 1])
 
-        for i in range(1, len(s1)):
-            for j in range(1, len(s2)):
-                if s1[i] == s2[i]:
+        for i in range(1, len(s1) + 1):
+            for j in range(1, len(s2) + 1):
+                if s1[i - 1] == s2[j - 1]:
                     dp[j][i] = dp[j - 1][i - 1]
                 else:
-                    dp[j][i] = min(dp[j - 1][i] + ord(s2[j]), dp[j][i - 1] + ord(s1[i]))
+                    dp[j][i] = min(dp[j - 1][i] + ord(s2[j - 1]), dp[j][i - 1] + ord(s1[i - 1]))
 
         return dp[-1][-1]
 
