fdca8b7e20680b400716b6db67bc29beba293e62
SatoShun <shun.sato1@gmail.com>
2019-11-13 00:45:03 +0000

    max consective

--- 1004.max-consecutive-ones-iii.py
+++ 1004.max-consecutive-ones-iii.py
@@ -9,74 +9,19 @@ from typing import List
 # @lc code=start
 class Solution:
     def longestOnes(self, A: List[int], K: int) -> int:
-        zero_group = {}
-        group = []
-        for i, a in enumerate(A):
-            if a == 0:
-                group.append(i)
-            else:
-                if group:
-                    zero_group[len(zero_group)] = group
-                    group = []
+        start, res = 0, 0
 
-        cache = []
+        for end in range(len(A)):
+            if A[end] == 0:
+                K -= 1
 
-        max_count = 0
+            while K < 0:
+                if A[start] == 0:
+                    K += 1
+                start += 1
 
-        def dfs(remain, position, visited):
-            nonlocal max_count
+            res = max(res, end - start + 1)
 
-            if position < 0 or position >= len(zero_group):
-                return
-            if visited in cache:
-                return
-            if remain == 0:
-                m = 0
-                for a in A:
-                    if a == 0:
-                        max_count = max(m, max_count)
-                        m = 0
-                    else:
-                        m += 1
+        return res
 
-            cache.append(visited)
-            group = zero_group[position]
-
-            # fill up and down
-            if len(group) <= remain:
-                for g in group:
-                    A[g] = 1
-                dfs(remain - len(group), position + 1, visited + [position])
-                dfs(remain - len(group), position - 1, visited + [position])
-                for g in group:
-                    A[g] = 0
-            else:
-                for g in group[:remain]:
-                    A[g] = 1
-                m = 0
-                for a in A:
-                    if a == 0:
-                        max_count = max(m, max_count)
-                        m = 0
-                    else:
-                        m += 1
-                for g in group[:remain]:
-                    A[g] = 0
-
-                for g in group[len(group) - remain:]:
-                    A[g] = 1
-                m = 0
-                for a in A:
-                    if a == 0:
-                        max_count = max(m, max_count)
-                        m = 0
-                    else:
-                        m += 1
-                for g in group[len(group) - remain:]:
-                    A[g] = 0
-
-        for i in range(len(zero_group)):
-            dfs(K, i, [])
-
-        return max_count
 # @lc code=end
