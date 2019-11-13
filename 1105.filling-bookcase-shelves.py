e803a4047dc3214c065c18e7939f3f023914c9c0
SatoShun <shun.sato1@gmail.com>
2019-11-13 22:56:47 +0000

    filling bookcase shelves

--- 1105.filling-bookcase-shelves.py
+++ 1105.filling-bookcase-shelves.py
@@ -10,43 +10,21 @@ import sys
 # @lc code=start
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelf_width: int) -> int:
-        min_height = sys.maxsize
+        dp = [sys.maxsize] * (len(books) + 1)
+        dp[0] = 0
 
-        cache = {}
+        for i in range(1, len(dp)):
+            width = books[i - 1][0]
+            height = books[i - 1][1]
+            dp[i] = dp[i - 1] + height
 
-        def search(remains, width, height, book_height):
-            nonlocal min_height
-            if not remains:
-                min_height = min(min_height, height)
-                return
+            j = i - 1
+            while j > 0 and width + books[j - 1][0] <= shelf_width:
+                height = max(height, books[j - 1][1])
+                width += books[j - 1][0]
+                dp[i] = min(dp[i], dp[j - 1] + height)
+                j -= 1
 
-            if height in cache:
-                if cache[height] < len(remains):
-                    return
-
-            remain = remains[0]
-            # next height
-            if remain[0] + width > shelf_width:
-                search(remains[1:], remain[0], height + remain[1], remain[1])
-                cache[height + remain[1]] = len(remains) - 1
-                return
-
-            # next width
-            if remain[1] <= book_height:
-                search(remains[1:], width + remain[0], height, book_height)
-                return
-
-            # next height or next width
-            search(remains[1:], remain[0], height + remain[1], remain[1])
-            cache[height + remain[1]] = len(remains) - 1
-            search(remains[1:], width + remain[0], height - book_height + remain[1], remain[1])
-            return
-
-        if not books:
-            return 0
-
-        search(books, 0, 0, 0)
-
-        return min_height
+        return dp[-1]
 
 # @lc code=end
