dd730d7166749d0628ccbdde5aede3218e71dded
SatoShun <shun.sato1@gmail.com>
2019-11-17 03:42:02 +0000

    Find Elements in a Contaminated Binary Tree

--- 163/163_2.py
+++ 163/163_2.py
@@ -9,35 +9,35 @@ class FindElements:
     def __init__(self, root: TreeNode):
         root.value = 0
         nodes = [root]
+        values = []
         while nodes:
             new_nodes = []
             for node in nodes:
                 if node.left:
                     node.left.value = node.value * 2 + 1
+                    values.append(node.value * 2 + 1)
                     new_nodes.append(node.left)
 
                 if node.right:
                     node.right.value = node.value * 2 + 2
+                    values.append(node.value * 2 + 2)
                     new_nodes.append(node.right)
             nodes = new_nodes
 
+        self.values = values
         self.root = root
 
     def find(self, target: int) -> bool:
-        nodes = [self.root]
-        while nodes:
-            new_nodes = []
-            for node in nodes:
-                if node.value == target:
-                    return True
-                if target < node.value:
-                    continue
-                if node.left:
-                    new_nodes.append(node.left)
-                if node.right:
-                    new_nodes.append(node.right)
-
-            nodes = new_nodes
+        start = 0
+        end = len(self.values)
+        while start < end:
+            mid = (end - start) // 2
+            if self.values[mid] == target:
+                return True
+            elif self.values[mid] > target:
+                end = mid
+            else:
+                start = mid + 1
 
         return False
 
