1793201e9110b465c57074b9ca7ca543e9ccbd8a
SatoShun <shun.sato1@gmail.com>
2019-11-17 23:54:33 +0000

    array nesting

--- 565.array-nesting.py
+++ 565.array-nesting.py
@@ -3,10 +3,26 @@
 #
 # [565] Array Nesting
 #
+from typing import List
+
 
 # @lc code=start
 class Solution:
     def arrayNesting(self, nums: List[int]) -> int:
-        pass
-# @lc code=end
+        visited = []
+        m = 0
+        for s in range(len(nums)):
+            if s in visited:
+                continue
 
+            current = 0
+            while s not in visited:
+                current += 1
+                visited.append(s)
+                s = nums[s]
+
+            m = max(m, current)
+
+        return m
+
+# @lc code=end
