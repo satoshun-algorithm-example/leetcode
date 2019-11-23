5e869b38b703a1a0a7fda35257c6c168e8895b3d
SatoShun <shun.sato1@gmail.com>
2019-11-23 03:40:34 +0000

    add memo difference ways

--- 241.different-ways-to-add-parentheses.py
+++ 241.different-ways-to-add-parentheses.py
@@ -8,15 +8,21 @@ from typing import List
 
 # @lc code=start
 class Solution:
-    def diffWaysToCompute(self, input: str) -> List[int]:
+    def diffWaysToCompute(self, input: str, memo=None) -> List[int]:
+        if memo is None:
+            memo = {}
+
         if input.isdigit():
             return [int(input)]
+
+        if input in memo:
+            return memo[input]
 
         res = []
         for i in range(len(input)):
             if input[i] in '-+*':
-                left = self.diffWaysToCompute(input[:i])
-                right = self.diffWaysToCompute(input[i + 1:])
+                left = self.diffWaysToCompute(input[:i], memo)
+                right = self.diffWaysToCompute(input[i + 1:], memo)
                 for l in left:
                     for r in right:
                         if input[i] == '-':
@@ -26,6 +32,7 @@ class Solution:
                         else:
                             res.append(l * r)
 
+        memo[input] = res
         return res
 
 # @lc code=end
