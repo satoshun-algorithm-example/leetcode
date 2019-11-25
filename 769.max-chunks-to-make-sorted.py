141a1b3326905e5d339655db7810cf17023aa0bf
SatoShun <shun.sato1@gmail.com>
2019-11-25 00:40:12 +0000

    max chunks

--- 769.max-chunks-to-make-sorted.py
+++ 769.max-chunks-to-make-sorted.py
@@ -9,11 +9,12 @@ from typing import List
 # @lc code=start
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        i = 0
         chunks = 0
-        while i < len(arr):
-            chunks += 1
-            i = arr[i] + 1
+        m = 0
+        for i in range(len(arr)):
+            m = max(arr[i], m)
+            if m == i:
+                chunks += 1
 
         return chunks
 
