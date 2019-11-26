bc2ccd0fee044de1ad7f244c96b077495af3df25
SatoShun <shun.sato1@gmail.com>
2019-11-26 11:49:35 +0000

    Minimum Moves to Equal Array Elements II

--- 462.minimum-moves-to-equal-array-elements-ii.py
+++ 462.minimum-moves-to-equal-array-elements-ii.py
@@ -10,29 +10,6 @@ from typing import List
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
-        count = 0
-        start = 0
-        end = len(nums) - 1
-        while len(nums) > start + 1 and nums[start] == nums[start + 1]:
-            start += 1
-        while end >= 0 and nums[end] == nums[end - 1]:
-            end -= 1
-
-        while start < end:
-            diff_start = (start + 1) * (nums[start + 1] - nums[start])
-            diff_end = (len(nums) - end) * (nums[end] - nums[end - 1])
-            if diff_start > diff_end:
-                end -= 1
-                count += diff_end
-            else:
-                start += 1
-                count += diff_start
-
-            while len(nums) > start + 1 and nums[start] == nums[start + 1]:
-                start += 1
-            while end > 0 and nums[end] == nums[end - 1]:
-                end -= 1
-
-        return count
-
+        median = nums[len(nums) // 2]
+        return sum(abs(num - median) for num in nums)
 # @lc code=end
