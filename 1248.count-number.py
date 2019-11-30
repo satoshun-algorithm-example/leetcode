de9cf009301d4b179cf71bc36a1bc29d7aa67466
SatoShun <shun.sato1@gmail.com>
2019-11-30 03:01:04 +0000

    simplify code

--- 1248.count-number.py
+++ 1248.count-number.py
@@ -7,32 +7,23 @@ from typing import List
 # @lc code=start
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        total = 0
+        nums = [n % 2 for n in nums]
 
-        start, c = 0, 0
-        n = 0
-        # calculate k sub array
-        for i in range(len(nums)):
-            c += nums[i] % 2
-            n += 1
-            while c > k:
-                c -= nums[start] % 2
-                start += 1
-                n -= 1
-            total += n
+        def count_up(k):
+            total = 0
 
-        start, c = 0, 0
-        n = 0
-        # calculate k - 1 sub array
-        for i in range(len(nums)):
-            c += nums[i] % 2
-            n += 1
-            while c > k - 1:
-                c -= nums[start] % 2
-                start += 1
-                n -= 1
-            total -= n
+            start, c = 0, 0
+            n = 0
+            for i in range(len(nums)):
+                c += nums[i]
+                n += 1
+                while c > k:
+                    c -= nums[start]
+                    start += 1
+                    n -= 1
+                total += n
+            return total
 
-        return total
+        return count_up(k) - count_up(k - 1)
 
 # @lc code=end
