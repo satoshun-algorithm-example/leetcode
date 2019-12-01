dbe1972c5df7c7537f9cf25f56cf9dabd1b139d7
SatoShun <shun.sato1@gmail.com>
2019-12-01 03:40:26 +0000

    5276. Number of Burgers with No Waste of Ingredients

--- 165/165_2.py
+++ 165/165_2.py
@@ -3,18 +3,16 @@ from typing import List
 
 class Solution:
     def numOfBurgers(self, tomatoSlices: int, cheeseSlices: int) -> List[int]:
-        jumbo = 0
-        small = 0
+        jumbo = (tomatoSlices - (2 * cheeseSlices)) / 2
+        if jumbo < 0:
+            return []
+        if int(jumbo) != jumbo:
+            return []
 
-        while not (tomatoSlices == 0 and cheeseSlices == 0):
-            if tomatoSlices == cheeseSlices * 2:
-                small = cheeseSlices
-                break
+        jumbo = int(jumbo)
 
-            tomatoSlices -= 4
-            cheeseSlices -= 1
-            jumbo += 1
-            if cheeseSlices < 0 or tomatoSlices < 0:
-                return []
+        small = cheeseSlices - jumbo
+        if small < 0:
+            return []
 
         return [jumbo, small]
