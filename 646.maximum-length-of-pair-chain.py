6176e35dd8d2d147ef6ed4b794ad985691edd46a
SatoShun <shun.sato1@gmail.com>
2019-12-05 00:45:27 +0000

    Maximum Length of Pair Chain

--- 646.maximum-length-of-pair-chain.py
+++ 646.maximum-length-of-pair-chain.py
@@ -9,18 +9,18 @@ from typing import List
 # @lc code=start
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
-        pairs = sorted(pairs, key=lambda a: a[1])
+        pairs.sort(key=lambda a: a[1])
         count = 1
         next_value = pairs[0][1]
         pairs = pairs[1::]
         while True:
             find = False
-            for pair in pairs:
+            for i, pair in enumerate(pairs):
                 if next_value < pair[0]:
-                    # TODO remove from pairs
                     next_value = pair[1]
                     find = True
                     count += 1
+                    pairs = pairs[i+1::]
                     break
 
             if not find:
