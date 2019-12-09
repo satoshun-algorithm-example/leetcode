a34cc50c296d3de5fb68abf6c22a618ba833474b
SatoShun <shun.sato1@gmail.com>
2019-12-09 01:08:38 +0000

    Maximum Length of Pair Chain

--- 646.maximum-length-of-pair-chain.py
+++ 646.maximum-length-of-pair-chain.py
@@ -12,11 +12,10 @@ class Solution:
         pairs.sort(key=lambda a: a[1])
         dp = [1 for _ in pairs]
         for i in range(1, len(pairs)):
-            dp[i] = max(dp)
             for j in range(i):
                 if pairs[j][1] < pairs[i][0]:
                     dp[i] = max(dp[i], dp[j] + 1)
 
-        return dp[-1]
+        return max(dp)
 
 # @lc code=end
