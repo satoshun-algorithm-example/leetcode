967d769d121f60a849997c5723d0811b6dfdf3b6
SatoShun <shun.sato1@gmail.com>
2019-12-17 00:41:23 +0000

    redundant connection

--- 684.redundant-connection.py
+++ 684.redundant-connection.py
@@ -14,21 +14,19 @@ class Solution:
         def find(x):
             if parent[x] == 0:
                 return x
-            parent[x] = find(parent[x])
-            return parent[x]
+            return find(parent[x])
 
         def union(x, y):
             root_x = find(x)
             root_y = find(y)
-
             if root_x == root_y:
                 return False
 
             parent[root_x] = root_y
             return True
 
-        for x, y in edges:
-            if not union(x - 1, y - 1):
-                return [x, y]
+        for edge in edges:
+            if not union(edge[0] - 1, edge[1] - 1):
+                return edge
 
 # @lc code=end
