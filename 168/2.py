6c4b3572115665feae6da60f69478b594f402661
SatoShun <shun.sato1@gmail.com>
2019-12-22 03:00:17 +0000

    Divide Array in Sets of K Consecutive Numbers

--- 168/2.py
+++ 168/2.py
@@ -3,21 +3,18 @@ from typing import List
 
 class Solution:
     def isPossibleDivide(self, nums: List[int], k: int) -> bool:
-        nums.sort()
         if len(nums) % k != 0:
             return False
+
+        nums.sort()
 
         while nums:
             start = nums.pop(0)
             for i in range(k - 1):
-                find = False
-                for j in range(len(nums)):
-                    if nums[j] == start + 1:
-                        start = nums.pop(j)
-                        find = True
-                        break
-
-                if not find:
+                try:
+                    index = nums.index(start + 1)
+                    start = nums.pop(index)
+                except:
                     return False
 
         return True
