9136a1aeba3ae25304f1f0c9333feae35702bb99
SatoShun <shun.sato1@gmail.com>
2020-01-02 01:51:11 +0000

    subarray sum

--- 560.subarray-sum-equals-k.py
+++ 560.subarray-sum-equals-k.py
@@ -9,6 +9,7 @@ from typing import List
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         cache = {0: 1}
+
         res = 0
         total = 0
         for num in nums:
