8d2c61e68c03fb0a8432581809f5184ddc284857
SatoShun <shun.sato1@gmail.com>
2020-01-04 14:03:30 +0000

    find k pairs

--- 373.find-k-pairs-with-smallest-sums.py
+++ 373.find-k-pairs-with-smallest-sums.py
@@ -9,26 +9,23 @@ from typing import List
 # @lc code=start
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        i1 = 0
-        i2 = 0
-        res = [[nums1[i1], nums1[i2]]]
+        res = []
+        heap = []
 
-        for i in range(k):
-            if i1 >= len(nums1):
-                res.append([nums1[i1], nums2[i2 + 1]])
-                i2 += 1
-                continue
-            if i2 >= len(nums2):
-                res.append([nums1[i1 + 1], nums2[i2]])
-                i1 += 1
-                continue
+        if not nums1 or not nums2:
+            return res
 
-            if nums1[i1 + 1] + nums2[i2] < nums1[i1] + nums2[i2 + 1]:
-                res.append([nums1[i1 + 1], nums2[i2]])
-                i1 += 1
-            else:
-                res.append([nums1[i1], nums2[i2 + 1]])
-                i2 += 1
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                heap.append([nums1[i], nums2[j]])
+
+            heap.sort(key=lambda a: a[0] + a[1])
+            res.append(heap.pop(0))
+            if len(res) == k:
+                return res
+
+        while len(res) != k and heap:
+            res.append(heap.pop(0))
 
         return res
 # @lc code=end
